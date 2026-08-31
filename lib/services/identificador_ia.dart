ca16e6a1e0839b1e0878bac8ce67c943eb179597
Jhon199000 <vilmarbatista1990@gmail.com>
2026-08-31 20:55:08 -0300

    Add files via upload

---
+++ lib/services/identificador_ia.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
+
+class IdentificadorIA {
+  final ImageLabeler _labeler = ImageLabeler(
+    options: ImageLabelerOptions(confidenceThreshold: 0.4),
+  );
+
+  Future<List<ImageLabel>> identificar(File imagem) async {
+    final inputImage = InputImage.fromFilePath(imagem.path);
+    return _labeler.processImage(inputImage);
+  }
+
+  void dispose() {
+    _labeler.close();
+  }
+}
